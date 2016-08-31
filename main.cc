8656f0cedcb3bc851544918a65eecb524cf84cfb
tim <tim@Narwhal.local>
2016-08-31 19:45:41 -0400

    surf detector

--- main.cc
+++ main.cc
@@ -1,134 +1,84 @@
-#include <string>
+#include <opencv2/core.hpp>
+#include <opencv2/xfeatures2d.hpp>
+#include <opencv2/features2d.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/calib3d.hpp>
 #include <iostream>
 #include <vector>
-#include <algorithm>
-#include <boost/filesystem.hpp>
-#include <opencv2/opencv.hpp>
-using namespace std;
 using namespace cv;
-using namespace cv::ml;
-using namespace boost;
+using namespace cv::xfeatures2d;
+using namespace std;
+int main(int, char* argv[2]) {
+	cv::Mat img_object = cv::imread(argv[1], CV_LOAD_IMAGE_GRAYSCALE);
+	cv::Mat img_scene = cv::imread(argv[2], CV_LOAD_IMAGE_GRAYSCALE);
+  int minHessian = 400;
+	cv::Ptr<cv::xfeatures2d::SURF> detector = cv::xfeatures2d::SURF::create(minHessian);
+	vector<cv::KeyPoint> kps1,kps2;
+	detector->detect(img_object,kps1);
+	detector->detect(img_scene,kps2);
+	std::cout << kps1.size() << ',' << kps2.size() << std::endl;
+	Mat dsp1,dsp2;
+	Ptr<SURF> extractor = SURF::create();
+	extractor->compute(img_object,kps1,dsp1);
+	extractor->compute(img_scene,kps2,dsp2);
+	cv::FlannBasedMatcher matcher;
+	vector<cv::DMatch> matches;
+	matcher.match(dsp1,dsp2,matches);
 
-std::vector<cv::Mat> LoadSampleImages(const std::string& directory) {
-	filesystem::path dir_path{directory};
-	std::vector<cv::Mat> samples;
-	if (filesystem::is_directory(dir_path)) {
-		for (auto&& path : filesystem::directory_iterator(dir_path)) {
-			if (filesystem::is_regular_file(path)) {
-				auto filename = path.path().relative_path().string();
-				auto m = cv::imread(filename, 0);
-				if (m.empty()) continue;
-				samples.push_back(std::move(m));
-			}
-		}
-	}
-	return samples;
-}
-
-void GetSVMDetector(const Ptr<SVM>& svm, vector<float>& hog_detector)
-{
-	// get the support vectors
-	Mat sv = svm->getSupportVectors();
-	const int sv_total = sv.rows;
-	// get the decision function
-	Mat alpha, svidx;
-	double rho = svm->getDecisionFunction(0, alpha, svidx);
-
-	CV_Assert(alpha.total() == 1 && svidx.total() == 1 && sv_total == 1);
-	CV_Assert((alpha.type() == CV_64F && alpha.at<double>(0) == 1.) ||
-		(alpha.type() == CV_32F && alpha.at<float>(0) == 1.f));
-	CV_Assert(sv.type() == CV_32F);
-	hog_detector.clear();
-
-	hog_detector.resize(sv.cols + 1);
-	memcpy(&hog_detector[0], sv.ptr(), sv.cols*sizeof(hog_detector[0]));
-	hog_detector[sv.cols] = (float)-rho;
-}
-
-Mat ComputeHog(const std::vector<cv::Mat> & samples, cv::Size size) {
-	Mat train_data;
-	for (auto img_raw : samples) {
-		cv::resize(img_raw, img_raw, size);
-		cv::HOGDescriptor d;
-		d.winSize = size;
-		std::vector<float> despv;
-		std::vector<cv::Point> locations;
-		try {
-		d.compute(img_raw, despv, cv::Size(8,8), cv::Size(0,0), locations);
-		}
-		catch(std::exception& e) {
-			std::cerr << e.what() << endl;
-			return train_data;
-		}
-		auto row = Mat{1,static_cast<int>(despv.size()),CV_32FC1,despv.data()};
-		train_data.push_back(row);
-	}
-	return train_data;
-}
-
-void train_svm(const Mat& train_data,
-		const std::vector<int>& labels,
-		const std::string& modelfile) {
-	/* Default values to train SVM */
-	cv::Ptr<SVM> svm = SVM::create();
-	svm->setCoef0(0.0);
-	svm->setDegree(3);
-	svm->setTermCriteria(cv::TermCriteria(cv::TermCriteria::MAX_ITER, 1000, 1e-3));
-	svm->setGamma(0);
-	svm->setKernel(SVM::LINEAR);
-	svm->setNu(0.5);
-	svm->setP(0.1); // for EPSILON_SVR, epsilon in loss function?
-	svm->setC(0.01); // From paper, soft classifier
-	svm->setType(SVM::ONE_CLASS); // C_SVC; // EPSILON_SVR; // may be also NU_SVR; // do regression task
-	svm->train(train_data, ROW_SAMPLE, cv::Mat{labels});
-	svm->save(modelfile);
-}
-
-void VehicleDetect(const Size& size,
-		const std::string& videoname,
-		const std::string& modelname) {
-		
-	auto hog = HOGDescriptor{};
-	hog.winSize = size;
-	auto svm = StatModel::load<SVM>(modelname);
-	auto video = VideoCapture{videoname};
-	auto hog_detector = vector<float>{};
-	GetSVMDetector(svm, hog_detector);
-	hog.setSVMDetector(hog_detector);
-
-	while (true) {
-		auto img = Mat{};
-		video.read(img);
-		if (img.empty()) break;
-		auto draw = Mat{img};
-		auto locations = vector<Rect>{};
-		cv::cvtColor(img, img, cv::COLOR_BGR2GRAY);
-		hog.detectMultiScale(img, locations);
-		if (!locations.empty()) {
-			for (const auto& box : locations) {
-				rectangle(draw, box, Scalar(0,0,255),2);
-			}
-		}
-		imshow(__func__,draw);
-		auto key = waitKey(1);
-		if (key == 'k') break;
-	}
-}
-
-int main(int argc, char* argv[])
-{
-  if (argc < 2)
-  {
-    cout << "Usage: test path\n";
-    return 1;
+  double max_dist = 0; double min_dist = 100;
+  //-- Quick calculation of max and min distances between keypoints
+  for (int i = 0; i < dsp1.rows; i++) {
+		double dist = matches[i].distance;
+    if(dist < min_dist) min_dist = dist;
+    if(dist > max_dist) max_dist = dist;
   }
-	//auto size = cv::Size(24,16);
-	auto size = cv::Size(32,32);
-  auto samples = LoadSampleImages(argv[1]);
-	std::vector<int> labels(samples.size(), 1);
-	auto despv = ComputeHog(samples, size);
-	std::string modelfile = "model.yml";
-	train_svm(despv, labels, modelfile);
-	VehicleDetect(size, argv[2], modelfile);
-  return 0;
+
+  //-- Draw only "good" matches (i.e. whose distance is less than 3*min_dist )
+	std::vector<cv::DMatch> good_matches;
+	for(int i = 0; i < dsp1.rows; i++) {
+		//std::cout << matches[i].distance <<" | "<< min_dist <<" | "<<max_dist<< std::endl;
+		if (matches[i].distance < 3*min_dist) {
+			good_matches.push_back(matches[i]);
+		}
+  }
+  Mat img_matches;
+  drawMatches( img_object, kps1, img_scene, kps2,
+               good_matches, img_matches, Scalar::all(-1), Scalar::all(-1),
+               vector<char>(), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS );
+#if 1
+  //-- Localize the object
+  std::vector<Point2f> obj;
+  std::vector<Point2f> scene;
+
+  for(size_t i = 0; i < good_matches.size(); i++ )
+  {
+    //-- Get the keypoints from the good matches
+    obj.push_back( kps1[good_matches[i].queryIdx].pt );
+    scene.push_back( kps2[good_matches[i].trainIdx].pt );
+  }
+	//std::cout << obj.size() << std::endl;	
+	//std::cout << scene.size() << std::endl;	
+
+  Mat H = findHomography(obj, scene,cv::RANSAC);
+  std::vector<Point2f> obj_corners(4);
+  obj_corners[0] = cvPoint(0,0);
+	obj_corners[1] = cvPoint( img_object.cols, 0 );
+  obj_corners[2] = cvPoint( img_object.cols, img_object.rows );
+	obj_corners[3] = cvPoint( 0, img_object.rows );
+  std::vector<Point2f> scene_corners(4);
+	perspectiveTransform( obj_corners, scene_corners, H);
+	/*
+  line( img_scene, scene_corners[0], scene_corners[1], Scalar(0, 255, 0), 4 );
+  line( img_scene, scene_corners[1], scene_corners[2], Scalar( 0, 255, 0), 4 );
+  line( img_scene, scene_corners[2], scene_corners[3], Scalar( 0, 255, 0), 4 );
+  line( img_scene, scene_corners[3], scene_corners[0], Scalar( 0, 255, 0), 4 );
+	*/
+	cv::Rect bb = cv::boundingRect(scene_corners);
+	cv::rectangle(img_scene,bb,Scalar(0,255,0),4);
+	imshow("x",img_scene);
+#endif
+	//imshow("x",img_matches);
+	cv::waitKey(0);
+	return 0;
 }
